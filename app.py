bdcf7cb99088c2a1955ca3e8785bc907d621dde4
Antonio Evandro <100598553+AntonioEvandro@users.noreply.github.com>
2023-05-16 21:40:07 -0300

    commit fim da aula

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -7,4 +7,6 @@ df = pd.read_csv("relatorio-sisref-sad - relatorio-sisref-sad.csv")
 
 st.title("Visualização de dados do relatorio so SISREF", anchor=False)
 st.write("Tabela", df)
+
+fig, ax = plt.subplots()
 
